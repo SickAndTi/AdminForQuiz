7a8c444e889cb17258bcd082b80ca3fb1b3a01e6
User <sickandti@mail.ru>
2018-10-30 11:30:02 +0300

    - signIn viaGoogle

--- app/src/main/java/com/scp/adminforquiz/ui/fragments/AuthFragment.java
+++ app/src/main/java/com/scp/adminforquiz/ui/fragments/AuthFragment.java
@@ -19,13 +19,9 @@ import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.Task;
 import com.scp.adminforquiz.R;
 import com.scp.adminforquiz.mvp.AuthPresenter;
 import com.scp.adminforquiz.mvp.AuthView;
@@ -34,7 +30,7 @@ import com.scp.adminforquiz.ui.adapters.AuthPagerAdapter;
 import timber.log.Timber;
 
 public class AuthFragment extends MvpAppCompatFragment implements AuthView {
-    private static final int RC_SIGN_IN = 1;
+    private static final int RC_SIGN_IN = 23;
     @InjectPresenter
     AuthPresenter authPresenter;
     Toolbar toolbar;
@@ -59,13 +55,12 @@ public class AuthFragment extends MvpAppCompatFragment implements AuthView {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         toolbar = view.findViewById(R.id.toolbar);
-        GoogleApiClient mGac = new GoogleApiClient.Builder(getActivity())
-                .enableAutoManage(getActivity() /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        Timber.d("ERROR");
-                    }
-                })
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity(), connectionResult -> Timber.d("ERROR"))
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
         toolbarTitle = view.findViewById(R.id.toolbarTitle);
@@ -76,10 +71,7 @@ public class AuthFragment extends MvpAppCompatFragment implements AuthView {
         googleImage = view.findViewById(R.id.googleImage);
         googleImage.setOnClickListener(v -> {
             authPresenter.regViaGoogle();
-//            GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
-//            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-
-            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGac);
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
             startActivityForResult(signInIntent, RC_SIGN_IN);
 
         });
@@ -99,45 +91,19 @@ public class AuthFragment extends MvpAppCompatFragment implements AuthView {
         }
     }
 
-
     @Override
     public void onStart() {
         super.onStart();
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
         Timber.d("account : %s", account);
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
-                .requestEmail()
-                .build();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            handleSignInResult(task);
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            Timber.d("RESULT:%s", result.getSignInAccount());
+            Timber.d("RESULT:%s", result.getSignInAccount().getEmail());
         }
     }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            Timber.d("account : %s", account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Timber.d("signInResult:failed code=%s", e.getStatusCode());
-//            updateUI(null);
-        }
-    }
-
 }
