39ad2eb733ae01ba1927222eedb47ea9dadf6bb4
User <sickandti@mail.ru>
2018-10-31 12:10:22 +0300

    - object for serever request

---
+++ app/src/main/java/com/scp/adminforquiz/CommonUserData.java
@@ -0,0 +1,11 @@
+package com.scp.adminforquiz;
+
+public class CommonUserData {
+    String id = null;
+    String email = null;
+    String firstName = null;
+    String secondName = null;
+    String lastName = null;
+    String fullName = null;
+    String avatarUrl = null;
+}
